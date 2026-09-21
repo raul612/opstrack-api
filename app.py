5a55411621b0ed87e48cb7a5879e71fe2dd91aed
Raul Corrêa <raul.correaa612@gmail.com>
2026-09-21 08:57:46 -0300

    style: aplicacao de linter no projeto

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/status')
 def status():
     return {'servico': 'OpsTrackAPI', 'status': 'online'}
+
 
 @app.route('/tickets')
 def tickets():
     return {'tickets': []}
 
+
 @app.route('/sobre')
 def sobre():
     return {'nome': 'OpsTrack API'}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
